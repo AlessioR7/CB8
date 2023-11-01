0b2a8f048abfd1c3b3022d0df1731752bd955aa4
AlessioR7 <148492232+AlessioR7@users.noreply.github.com>
2023-11-01 18:16:43 +0100

    Add files via upload

---
+++ Esercitazione 01-11-23/script.js
@@ -0,0 +1,14 @@
+const Animali = [
+  "Gatto",
+  "Cane",
+  "Elefante",
+  "Gallina",
+  "Ipopotamo",
+  "Aquila",
+  "Leone",
+  "Serpente",
+];
+
+for (let i = 0; i < Animali.length; i++) {
+  console.log(`${i + 1}: ${Animali[i]}`);
+}
